ee502c4787c25923abdb055d9e9c8bbec7b3a4c9
matheusmiguel-santos <51680772+matheusmiguel-santos@users.noreply.github.com>
2023-10-30 06:46:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const MercadoPago = require('mercadopago');
+const mercadopago = require('mercadopago');  // Note que aqui é tudo em minúsculas
 
 const app = express();
 
-// Para permitir qualquer origem (não recomendado para produção)
 app.use(cors());
+app.use(bodyParser.json());
 
-// Ou especificar a origem permitida
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
-}));
-
-// Substitua com suas credenciais do Mercado Pago
-MercadoPago.configure({
-  access_token: 'TEST-2684905602430236-052513-51d07b1caa42a7938ab7e2a9f13a7f98-135153905',
-  
+// Configure suas credenciais
+mercadopago.configure({  // Note que aqui é tudo em minúsculas
+  access_token: 'TEST-2684905602430236-052513-51d07b1caa42a7938ab7e2a9f13a7f98-135153905'
 });
 
 app.post('/create_payment', async (req, res) => {
-    const products = req.body;
-  
-    const order = {
-      // Adicione informações adicionais do pedido aqui, como ID do pedido, usuário, etc.
-    };
-  
-    const preference = {
-      external_reference: order.id, // o ID do seu pedido
-      items: products.map(product => ({
-        title: product.title,
-        unit_price: product.price,
-        quantity: product.quantity,
-      })),
-    };
-  
-    try {
-      const payment = await MercadoPago.preferences.create(preference);
-      res.status(200).send({ id: payment.body.id });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send({ error: 'Creating payment failed' });
-    }
-  });
-  
-  
+  const products = req.body;
+
+  const preference = {
+    items: products.map(product => ({
+      title: product.title,
+      unit_price: parseFloat(product.price),
+      quantity: parseInt(product.quantity),
+    })),
+  };
+
+  try {
+    const payment = await mercadopago.preferences.create(preference);
+    res.status(200).send({ id: payment.body.id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Creating payment failed' });
+  }
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
