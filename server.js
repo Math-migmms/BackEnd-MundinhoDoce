e0640d2a915ba92c6b59cf4bdc6b5f2ab85dd6c5
matheusmiguel-santos <51680772+matheusmiguel-santos@users.noreply.github.com>
2023-10-30 07:17:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mercadopago = require('mercadopago');  // Note que aqui é tudo em minúsculas
+const mercadopago = require('mercadopago');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-// Configure suas credenciais
-mercadopago.configure({  // Note que aqui é tudo em minúsculas
-  access_token: 'TEST-2684905602430236-052513-51d07b1caa42a7938ab7e2a9f13a7f98-135153905'
+// Substitua com suas credenciais do Mercado Pago
+mercadopago.configure({
+  access_token: 'SEU_ACCESS_TOKEN_AQUI'
 });
 
 app.post('/create_payment', async (req, res) => {
-    const products = req.body;
-  
-    console.log("Produtos recebidos do frontend:", products);  // Log de Depuração
-  
-    const preference = {
-      items: products.map(product => ({
-        title: product.title,
-        unit_price: parseFloat(product.price),
-        quantity: parseInt(product.quantity),
-      })),
-    };
-  
-    try {
-      const payment = await mercadopago.preferences.create(preference);
-      console.log("Resposta da API do Mercado Pago:", payment);  // Log de Depuração
-      res.status(200).send({ id: payment.body.id });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send({ error: 'Creating payment failed' });
+  const products = req.body;
+
+  const preference = {
+    items: products.map(product => ({
+      title: product.title,
+      unit_price: product.price,
+      quantity: product.quantity,
+    })),
+    back_urls: {
+      success: 'http://localhost:3000/success',
+      failure: 'http://localhost:3000/failure',
+      pending: 'http://localhost:3000/pending',
     }
-  });
-  
+  };
+
+  try {
+    const payment = await mercadopago.preferences.create(preference);
+    console.log("Payment Preference: ", payment);
+    res.status(200).send({ id: payment.body.id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Creating payment failed' });
+  }
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
