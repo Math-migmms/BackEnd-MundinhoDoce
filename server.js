27f7c0f1bcfe9a1b991d36108d158a7a0499f7d3
matheusmiguel-santos <51680772+matheusmiguel-santos@users.noreply.github.com>
2023-10-30 05:55:55 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mercadopago = require('mercadopago');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Substitua com suas credenciais do Mercado Pago
+mercadopago.configure({
+  access_token: 'TEST-2684905602430236-052513-51d07b1caa42a7938ab7e2a9f13a7f98-135153905',
+  integrator_id: 'TEST-c3b08660-7c79-431e-aced-7dceeffec5f5'
+});
+
+app.post('/create_payment', async (req, res) => {
+  const { title, quantity, price } = req.body;
+
+  const preference = {
+    items: [
+      {
+        title,
+        unit_price: price,
+        quantity,
+      },
+    ],
+  };
+
+  try {
+    const payment = await mercadopago.preferences.create(preference);
+    res.status(200).send({ id: payment.body.id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'Creating payment failed' });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
